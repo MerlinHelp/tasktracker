062d86f89bd3681f4c0df4d5a28421e91200dd32
Richard Vo <richardvo2004@gmail.com>
2025-04-21 03:33:49 -0700

    Updated TODO list. Modified InputParser: an input parser should only parse through arguments, not run commands

--- src/main/InputParser.java
+++ src/main/InputParser.java
@@ -12,7 +12,7 @@ class InputParser {
         "tasktracker <command> <command-arg1> <command-arg2> ...\n" +
         "Use 'tasktracker help' for general help or" + 
         "'tasktracker <command> help' for command-specific help";
-    private final Set<String> validBaseArguments = new HashSet<>(
+    private final Set<String> validBaseCommands = new HashSet<>(
         List.of(
             "add",
             "update",
@@ -29,14 +29,14 @@ class InputParser {
     }
 
 
-    public void runCommand() {
+    public boolean isValidBaseCommand() {
         if (input.length == 0) {
             throw new IllegalArgumentException(
                     String.format(
                         "Incorrect usage: No Arguments Found\n%s",
                         correctUsage
                     ));
-        } else if (!isValidArgument(input[0])) {
+        } else if (input[0] == null || !validBaseCommands.contains(input[0])) {
             String incorrectCommand = input[0].length() > 10 ? input[0].substring(0, 10) + "..." : input[0];
             throw new IllegalArgumentException(
                     String.format(
@@ -45,16 +45,6 @@ class InputParser {
                         correctUsage
                     ));
         }
-
+        return true;
     }
-
-    public boolean isValidArgument(String command) {
-        return command != null && validBaseArguments.contains(command);
-        
-    }
-    
-    public String getValidArguments() {
-        return "";
-    }
-    
 }
